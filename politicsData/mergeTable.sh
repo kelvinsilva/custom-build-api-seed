a7c9f99b19e368f51ec71d43a4857008dff53dd3
Kelvin OG <kelvinsilva747@gmail.com>
2015-04-11 03:25:17 +0000

    adding curl scripts to test

--- politicsData/mergeTable.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-cat liberalsTableClean.txt  > politicsData.txt
-tail -n+2 republicanTableClean.txt >> politicsData.txt
--- politicsData/parseLiberals.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-awk 'BEGIN {FS = ","; print "party%%%createdUtc%%%score%%%domain%%%title%%%author%%%ups%%%downs%%%numComments%%%permaLinks%%%selfText%%%thumbNail%%%urlImg" > "tableTest.txt" }   {if ($0 ~ /^[0-9].*,$/) 
-							{ 
-								if ($0 ~ /".*,.*"/) ; 
-									else printf "Liberal%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s%%%%%%%-s\n", $1, $2, $3, $5, $6, $7, $8, $9, $10, $11, $14, $21 >> "tableTest.txt"
-							}	 
-						}' Liberal.csv
