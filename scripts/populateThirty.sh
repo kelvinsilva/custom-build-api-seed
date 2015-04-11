11d827c9fbc268e32a962c4dd6f72ee8b2d8154a
Kelvin OG <kelvinsilva747@gmail.com>
2015-04-11 06:22:53 +0000

    delete scripts folder, keep testScripts folder

--- scripts/populateThirty.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in {1..30};
-do 
-RANDOM_NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo $RANDOM_NAME
-curl -X POST -H "Content-Type: application/json" -d '{"name":"'"$RANDOM_NAME"'"}' http://localhost:5000/moneys/	
-done
